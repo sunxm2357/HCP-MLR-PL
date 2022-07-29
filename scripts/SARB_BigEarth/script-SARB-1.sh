9f60d3b544f6f5dd95392fa0c757793cec97f383
Ximeng Sun <sunxm@fb.com>
2022-07-29 13:57:17 -0700

    add big earth

---
+++ scripts/SARB_BigEarth/script-SARB-1.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+post='SARB-BigEarth'
+printFreq=1000
+
+mode='SARB'
+dataset='BigEarth'
+prob=1
+
+pretrainedModel='./data/checkpoint/resnet101.pth'
+resumeModel='None'
+evaluate='False'
+
+epochs=20
+startEpoch=0
+stepEpoch=10
+workers=8
+
+batchSize=8
+lr=1e-5
+momentum=0.9
+weightDecay=5e-4
+
+cropSize=448
+scaleSize=512
+workers=8
+
+mixupEpoch=5
+contrastiveLossWeight=0.05
+
+prototypeNum=10
+recomputePrototypeInterval=5
+
+isAlphaLearnable='True'
+isBetaLearnable='True'
+
+python SARB.py \
+    --post ${post} \
+    --printFreq ${printFreq} \
+    --mode ${mode} \
+    --dataset ${dataset} \
+    --prob ${prob} \
+    --pretrainedModel ${pretrainedModel} \
+    --resumeModel ${resumeModel} \
+    --evaluate ${evaluate} \
+    --epochs ${epochs} \
+    --startEpoch ${startEpoch} \
+    --stepEpoch ${stepEpoch} \
+    --batchSize ${batchSize} \
+    --lr ${lr} \
+    --momentum ${momentum} \
+    --weightDecay ${weightDecay} \
+    --cropSize ${cropSize} \
+    --scaleSize ${scaleSize} \
+    --workers ${workers} \
+    --mixupEpoch ${mixupEpoch} \
+    --contrastiveLossWeight ${contrastiveLossWeight} \
+    --prototypeNum ${prototypeNum} \
+    --recomputePrototypeInterval ${recomputePrototypeInterval} \
+    --isAlphaLearnable ${isAlphaLearnable} \
+    --isBetaLearnable ${isBetaLearnable} \
