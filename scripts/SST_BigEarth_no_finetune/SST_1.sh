1318a77480e179ec8cff37ff3f90d029132dcf9d
Ximeng Sun <sunxm@fb.com>
2022-07-29 11:02:44 -0700

    add big earth

---
+++ scripts/SST_BigEarth_no_finetune/SST_1.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+
+post='SST-BigEarth'
+printFreq=1000
+
+mode='SST'
+dataset='BigEarth'
+prob=1
+
+pretrainedModel='./data/checkpoint/resnet101.pth'
+resumeModel='None'
+evaluate='False'
+
+epochs=20
+startEpoch=0
+stepEpoch=10
+
+batchSize=8
+lr=1e-5
+momentum=0.9
+weightDecay=5e-4
+
+cropSize=448
+scaleSize=512 
+workers=8
+
+generateLabelEpoch=5
+
+intraBCEWeight=1.0
+intraBCEMargin=0.95
+intraCooccurrenceWeight=10.0
+
+interBCEWeight=1.0
+interBCEMargin=0.95
+interDistanceWeight=0.05
+interExampleNumber=100
+
+OMP_NUM_THREADS=8 MKL_NUM_THREADS=8 CUDA_VISIBLE_DEVICES=0 python SST.py \
+    --post ${post} \
+    --printFreq ${printFreq} \
+    --mode ${mode} \
+    --dataset ${dataset} \
+    --prob ${prob} \
+    --pretrainedModel ${pretrainedModel} \
+    --resumeModel ${resumeModel} \
+    --evaluate ${evaluate} \
+    --epochs ${epochs} \
+    --startEpoch ${startEpoch} \
+    --stepEpoch ${stepEpoch} \
+    --batchSize ${batchSize} \
+    --lr ${lr} \
+    --momentum ${momentum} \
+    --weightDecay ${weightDecay} \
+    --cropSize ${cropSize} \
+    --scaleSize ${scaleSize} \
+    --workers ${workers} \
+    --generateLabelEpoch ${generateLabelEpoch} \
+    --intraBCEMargin ${intraBCEMargin} \
+    --intraBCEWeight ${intraBCEWeight} \
+    --intraCooccurrenceWeight ${intraCooccurrenceWeight} \
+    --interBCEWeight ${interBCEWeight} \
+    --interBCEMargin ${interBCEMargin} \
+    --interDistanceWeight ${interDistanceWeight} \
+    --interExampleNumber ${interExampleNumber} \
